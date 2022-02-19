6658059596ef4e43d8ca5d11cdd9b76cd4015a8c
Ice-cream-NFT <98552378+Ice-cream-NFT@users.noreply.github.com>
2022-02-19 14:28:18 +0100

    Add files via upload

---
+++ mdp.py
@@ -0,0 +1,11 @@
+print ("ENTRER LE MOT DE PASSE    |")
+mdpe = input ("~$ ")
+mdp = "ice"
+mdpf = False
+
+if mdp == mdpe :
+	print ("Mot de passe juste")
+else :
+	while mdpf == False :
+		print ("Mot de passe faux")
+			
